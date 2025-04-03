5eb6ba631c577784f2eefcfba9a7d6b058d25b5e
aliassan <alimkwananzi@gmail.com>
2025-04-03 07:07:27 +0200

    feat: add create edit actions

--- Controllers/VenueConroller.cs
+++ Controllers/VenueConroller.cs
@@ -21,13 +21,54 @@ namespace EventEase.Controllers
         }
 
         // GET: Venues/Create
+        // public IActionResult Create()
+        // {
+        //     return View();
+        // }
+
+        // [HttpPost]
+        // public async Task<IActionResult> Create(Venue venue)
+        // {
+        //     if (ModelState.IsValid)
+        //     {
+        //         _context.Add(venue);
+        //         await _context.SaveChangesAsync();
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     return View(venue);
+        // }
+
+        // GET: Venues/Create
         public IActionResult Create()
         {
-            return View();
+            ViewData["FormAction"] = "Create";
+            ViewData["SubmitButtonText"] = "Create";
+            return View("CreateEdit", new Venue());
         }
 
+        // GET: Venues/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venue = await _context.Venues.FindAsync(id);
+            if (venue == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["FormAction"] = "Edit";
+            ViewData["SubmitButtonText"] = "Save";
+            return View("CreateEdit", venue);
+        }
+
+        // POST: Venues/Create
         [HttpPost]
-        public async Task<IActionResult> Create(Venue venue)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VenueId,VenueName,Location,Capacity,ImageUrl")] Venue venue)
         {
             if (ModelState.IsValid)
             {
@@ -35,9 +76,50 @@ namespace EventEase.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(venue);
+
+            ViewData["FormAction"] = "Create";
+            ViewData["SubmitButtonText"] = "Create";
+            return View("CreateEdit", venue);
+        }
+
+        // POST: Venues/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VenueId,VenueName,Location,Capacity,ImageUrl")] Venue venue)
+        {
+            if (id != venue.VenueId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VenueExists(venue.VenueId))
+                    {
+                        return NotFound();
+                    }
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["FormAction"] = "Edit";
+            ViewData["SubmitButtonText"] = "Save";
+            return View("CreateEdit", venue);
         }
 
         // Similar actions for Edit/Delete/Details
+
+        private bool VenueExists(int id)
+        {
+            return _context.Venues.Any(e => e.VenueId == id);
+        }
     }
 }

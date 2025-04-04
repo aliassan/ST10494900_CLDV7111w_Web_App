abcd60ba1f948d9302deb019848c64e2adba17ad
aliassan <alimkwananzi@gmail.com>
2025-04-04 17:58:05 +0200

    refactor: booking model

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,15 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EventEase.Models
 {
     public class Booking
     {
         public int BookingId { get; set; }
+
+        [Required]
+        [Display(Name = "Booking Date")]
         public DateTime BookingDate { get; set; } = DateTime.Now;
 
-        // Relationships
+        // Foreign Keys
+        [Required]
+        [Display(Name = "Event")]
         public int EventId { get; set; }
-        public Event? Event { get; set; }
 
+        [Required]
+        [Display(Name = "Venue")]
         public int VenueId { get; set; }
+
+        // Navigation Properties
+        public Event? Event { get; set; }
         public Venue? Venue { get; set; }
     }
 }

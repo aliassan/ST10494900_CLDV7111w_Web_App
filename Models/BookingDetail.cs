62b037bf91f62c80748607e9589356828c888956
aliassan <alimkwananzi@gmail.com>
2025-06-11 20:35:43 +0200

    feat: add new fields

--- Models/BookingDetail.cs
+++ Models/BookingDetail.cs
@@ -4,25 +4,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EventEase.Models
 {
-[Keyless] // Important for views
-public class BookingDetail
-{
-    public int BookingId { get; set; }
-    public DateTime BookingDate { get; set; }
-    
-    // Event Info
-    public int EventId { get; set; }
-    public required string EventName { get; set; }
-    public DateTime EventDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public string? EventDescription { get; set; }
-    public string? EventImageUrl { get; set; }
-    
-    // Venue Info
-    public int VenueId { get; set; }
-    public required string VenueName { get; set; }
-    public required string Location { get; set; }
-    public int Capacity { get; set; }
-    public string? VenueImageUrl { get; set; }
-}
+    [Keyless] // Important for views
+    public class BookingDetail
+    {
+        public int BookingId { get; set; }
+        public DateTime BookingDate { get; set; }
+        
+        // Event Info
+        public int EventId { get; set; }
+        public required string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? EventDescription { get; set; }
+        public string? EventImageUrl { get; set; }
+        
+        // New Event Type Info
+        public int? EventTypeId { get; set; }
+        public string? EventType { get; set; }
+        
+        // Venue Info
+        public int VenueId { get; set; }
+        public required string VenueName { get; set; }
+        public required string Location { get; set; }
+        public int Capacity { get; set; }
+        
+        // New Venue Availability
+        public bool IsAvailable { get; set; }
+        
+        public string? VenueImageUrl { get; set; }
+    }
 }
